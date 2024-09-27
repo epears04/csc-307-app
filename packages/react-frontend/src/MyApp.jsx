483c446b807504ef0ef7e37cd32eb3a5d80cb7df
Ellie Pearson <ellie.jm.pearson@gmail.com>
2024-09-26 20:16:31 -0700

    implemented saving of people

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -12,6 +12,10 @@ function MyApp() {
 		});
 		setCharacters(updated);
 	}
+
+	function updateList(person) {
+		setCharacters([...characters, person]);
+	}
   
 	return (
 	  	<div className="container">
@@ -19,9 +23,10 @@ function MyApp() {
 			characterData={characters}
 			removeCharacter={removeOneCharacter}
 		  />
-		  <Form />
+		  <Form handleSubmit={updateList} />
 		</div>
 	);
-  }
+}
+
 
 export default MyApp;
