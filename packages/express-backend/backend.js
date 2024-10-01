67b9c42a5293a29ef853203fd9da7f12e9750cac
Ellie Pearson <ellie.jm.pearson@gmail.com>
2024-10-01 10:21:42 -0700

    added findUserByID

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -47,6 +47,20 @@ const findUserByJob = (job) => {
     );
 };
 
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params.id;
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result);
+    }
+});
+
 app.get("/users", (req, res) => {
     const job = req.query.job;
     if (job != undefined) {
