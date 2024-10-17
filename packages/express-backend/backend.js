5dcdf801c3cd818e853a77015f127cfeffcb1276
Ellie Pearson <ellie.jm.pearson@gmail.com>
2024-10-16 21:42:07 -0700

    deleted unnecessary code

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -17,54 +17,6 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 
-const users = {
-    users_list: [
-      {
-        id: "xyz789",
-        name: "Charlie",
-        job: "Janitor"
-      },
-      {
-        id: "abc123",
-        name: "Mac",
-        job: "Bouncer"
-      },
-      {
-        id: "ppp222",
-        name: "Mac",
-        job: "Professor"
-      },
-      {
-        id: "yat999",
-        name: "Dee",
-        job: "Aspring actress"
-      },
-      {
-        id: "zap555",
-        name: "Dennis",
-        job: "Bartender"
-      }
-    ]
-};
-
-const findUserByName = (name) => {
-    return users["users_list"].filter(
-      (user) => user["name"] === name
-    );
-};
-
-const findUserByJob = (job) => {
-    return users["users_list"].filter(
-        (user) => user["job"] === job
-    );
-};
-
-const findUserByNameAndJob = (name, job) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name && user["job"] === job
-  );
-};
-
 const findUserById = (id) =>
     users["users_list"].find((user) => user["id"] === id);
 
