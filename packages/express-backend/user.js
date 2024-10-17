07bfb1c73ec1919efe64a99fb9e76e0510d74b42
Ellie Pearson <ellie.jm.pearson@gmail.com>
2024-10-16 21:29:48 -0700

    updated name so correct db connected

--- packages/express-backend/user.js
+++ packages/express-backend/user.js
@@ -18,7 +18,7 @@ const UserSchema = new mongoose.Schema(
             },
         },
     },
-    {collection: "users_lists"}
+    {collection: "users_list"}
 );
 
 const User = mongoose.model("User", UserSchema);
